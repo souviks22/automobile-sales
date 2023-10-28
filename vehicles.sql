781dfd76bd9613b3b04e24b82f87d963a7c9026c
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-10-28 15:05:13 +0530

    Vehicles relation modified

--- vehicles.sql
+++ vehicles.sql
@@ -1,6 +1,9 @@
 CREATE TABLE vehicles(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     vin CHAR(10) NOT NULL UNIQUE,
+    features VARCHAR(1000) NOT NULL,
     model_id INT,
-    FOREIGN KEY(model_id) REFERENCES models(id)
+    supplier_id INT,
+    FOREIGN KEY(model_id) REFERENCES models(id),
+    FOREIGN KEY(supplier_id) REFERENCES suppliers(id)
 );
