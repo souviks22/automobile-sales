53df6e88a2a36531e46f78031776b2f4dc54ab9a
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-10-30 14:38:19 +0530

    Customers, Dealers, Sales Data Insertion

--- src/dealers.sql
+++ src/dealers.sql
@@ -2,3 +2,9 @@ CREATE TABLE dealers(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     dealername VARCHAR(40) NOT NULL
 );
+
+INSERT INTO dealers(dealername)
+VALUES ('John Smith'),('Emily Johnson'),('Michael Davis'),('Sarah Brown'),('David Wilson'),
+('Jennifer Lee'),('Robert Taylor'),('Lisa Anderson'),('William Evans'),('Jessica Hall'),
+('Christopher Clark'),('Mary White'),('James Harris'),('Amanda Martinez'),('Joseph Lewis'),
+('Melissa Moore'),('Daniel Turner'),('Elizabeth Baker'),('Richard Parker'),('Nicole Young');
